2c5db557dcebeb1ec7f73960b4eea894fcbbbffb
e <1061210163@qq.com>
2020-04-14 15:57:31 +0800

    1

---
+++ code/rocketmq-demo/src/main/java/com/itheima/mq/rocketmq/batch/Producer2.java
@@ -0,0 +1,45 @@
+package com.itheima.mq.rocketmq.batch;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Producer2 {
+
+    public static void main(String[] args) throws Exception {
+        //1.创建消息生产者producer，并制定生产者组名
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+        //2.指定Nameserver地址
+        producer.setNamesrvAddr("192.168.3.156:9876;192.168.3.157:9876");
+        //3.启动producer
+        producer.start();
+
+
+        String topic = "BatchTopic";
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message(topic, "TagA", "OrderID001", "Hello world 0".getBytes()));
+        messages.add(new Message(topic, "TagA", "OrderID002", "Hello world 1".getBytes()));
+        messages.add(new Message(topic, "TagA", "OrderID003", "Hello world 2".getBytes()));
+        ListSplitter splitter = new ListSplitter(messages);
+        while (splitter.hasNext()) {
+            try {
+                List<Message>  listItem = splitter.next();
+                producer.send(listItem);
+            } catch (Exception e) {
+                e.printStackTrace();
+                //handle the error
+            }
+        }
+
+        //线程睡1秒
+        TimeUnit.SECONDS.sleep(1);
+
+
+        //6.关闭生产者producer
+        producer.shutdown();
+    }
+
+}
